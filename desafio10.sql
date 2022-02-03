3ea0db24138ae35282805e9ab09ff85e22d35a0e
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-02-03 17:12:16 -0300

    req10

---
+++ desafio10.sql
@@ -0,0 +1,9 @@
+SELECT 
+    rep.historico_de_reproducoes AS nome,
+    COUNT(rep.id_musica) AS reproducoes
+FROM
+    reproducoes rep
+INNER JOIN usuarios u ON u.id_usuario = rep.id_usuario
+WHERE u.id_plano IN(1, 4)
+GROUP BY rep.historico_de_reproducoes
+order by reproducoes DESC, nome;
