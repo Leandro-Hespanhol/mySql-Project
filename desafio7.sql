700735208faf61dc20501874b2d00315fcefe3c1
Leandro Hespanhol <leandrohespanhol@gmail.com>
2022-02-03 18:54:48 -0300

    req 06 sem case

--- desafio7.sql
+++ desafio7.sql
@@ -1,12 +1,10 @@
-select DISTINCT SEG.seguindo_artistas AS artista, ALB.album, 
-CASE 
- WHEN SEG.id_artista = 1 THEN 5
- WHEN SEG.id_artista = 2 THEN 4
- WHEN SEG.id_artista = 3 THEN 3
- WHEN SEG.id_artista = 4 THEN 4
- WHEN SEG.id_artista = 5 THEN 3
- WHEN SEG.id_artista = 6 THEN 3
- END AS seguidores
-from albums ALB
-INNER JOIN usuario_seg_artistas SEG ON SEG.id_artista = ALB.id_artista
-order by seguidores DESC, SEG.seguindo_artistas, ALB.album;
+SELECT 
+    SEG.seguindo_artistas AS artistas,
+    ALB.album,
+    COUNT(SEG.seguindo_artistas) AS seguidores
+FROM
+    albums ALB
+        INNER JOIN
+    usuario_seg_artistas SEG ON SEG.id_artista = ALB.id_artista
+GROUP BY ALB.album , SEG.seguindo_artistas
+ORDER BY seguidores DESC , SEG.seguindo_artistas , ALB.album;
